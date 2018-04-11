9e2a8d43baf28cc3a9fab116a33631a47080bab7
Bewotis <BENNY-9-7@t-online.de>
2018-04-11 14:14:30 +0200

    new rover repository

---
+++ rover.py
@@ -0,0 +1,76 @@
+class Rover:
+
+    def __init__(self, x, y, orientation):
+        self.x = x
+        self.y = y
+        self.orientation = orientation
+
+    def forward(self):
+        if self.orientation == 'N' or 'n':
+            self.x += 1
+
+        elif self.orientation == 'E' or 'e':
+            self.y += 1
+
+        elif self.orientation == 'S' or 's':
+            self.x -= 1
+
+        elif self.orientation == 'W' or 'w':
+            self.y -= 1
+
+    def backward(self):
+        if self.orientation == 'N' or 'n':
+            self.x -= 1
+
+        elif self.orientation == 'E' or 'e':
+            self.y -= 1
+
+        elif self.orientation == 'S' or 's':
+            self.x += 1
+
+        elif self.orientation == 'W' or 'w':
+            self.y += 1
+
+    def turnleft(self):
+        if self.orientation == 'N' or 'n':
+            self.orientation == 'W'
+
+        elif self.orientation == 'E' or 'e':
+            self.orientation == 'N'
+
+        elif self.orientation == 'S' or 's':
+            self.orientation == 'E'
+
+        elif self.orientation == 'W' or 'w':
+            self.orientation == 'S'
+
+    def turnright(self):
+        if self.orientation == 'N' or 'n':
+            self.orientation == 'E'
+
+        elif self.orientation == 'E' or 'e':
+            self.orientation == 'S'
+
+        elif self.orientation == 'S' or 's':
+            self.orientation == 'W'
+
+        elif self.orientation == 'W' or 'w':
+            self.orientation == 'N'
+
+    def readcommands(self,commands):    #function to read the commands and execute the corresponding functions
+        for x in commands:
+            if x == 'f':
+                self.forward(self)
+
+            if x == 'b':
+                self.backward(self)
+
+            if x == 'r':
+                self.turnright(self)
+
+            if x == 'l':
+                self.turnleft(self)
+
+    def giveroverlocation(self):
+        print('The rover is on %d : %d facing %s' % (self.x, self.y, self.orientation))
+
---
+++ rovertest.py
@@ -0,0 +1,9 @@
+from rover import Rover
+
+
+class RoverTest:
+
+    # initial creation of the roverobject
+    rover = Rover(0, 0, 'N')
+    Rover.giveroverlocation(rover)
+
