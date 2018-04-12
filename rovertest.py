378663e4fea830eb0cbe326b9106c1ef955a9961
Bewotis <BENNY-9-7@t-online.de>
2018-04-12 09:26:06 +0200

    added the test will rover refuse unknown commands

--- rovertest.py
+++ rovertest.py
@@ -10,7 +10,15 @@ class RoverTestCase(unittest.TestCase):
         givecommands = ['f', 'r', 'f', 'f']  # these are the commands that should be executed
         # Rover.readcommands(rover, givecommands)
         # Rover.giveroverlocation(rover)
-        self.assertTrue(Rover.readcommands(rover, givecommands))
+        self.assertTrue(Rover.readcommands(rover, givecommands), msg=Rover.giveroverlocation(rover))
+
+    def test_will_rover_refuse_unknown_commands(self):
+        rover = Rover(0, 0, 'N')
+
+        givecommands = ['s', 'n', 'a', 'r']  # these are the commands that should be executed
+        # Rover.readcommands(rover, givecommands)
+        # Rover.giveroverlocation(rover)
+        self.assertTrue(Rover.readcommands(rover, givecommands), msg=Rover.giveroverlocation(rover))
 
 
 if __name__ == '__main__':
