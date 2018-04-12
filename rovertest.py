88aaa8c2d78ab4abe59ef28a4496e0d61e5ec8ca
Bewotis <BENNY-9-7@t-online.de>
2018-04-12 11:14:39 +0200

    updated the unittests with setUp and tearDown

--- rovertest.py
+++ rovertest.py
@@ -5,32 +5,31 @@ from planet import Planet
 
 class RoverTestCase(unittest.TestCase):
     # initial creation of the roverobject
-    def test_will_rover_move(self):
-        planet = Planet(10, 10)
-        rover = Rover(0, 0, 'N', planet)
 
+    def setUp(self):  # creating same planet and rover for each test case
+        self.planet = Planet(10, 10)
+        self.rover = Rover(0, 0, 'N', self.planet)
+
+    def tearDown(self):
+        print('\n \n')
+
+    def test_will_rover_move(self):
         givecommands = ['f', 'r', 'f', 'f']  # these are the commands that should be executed
         # Rover.readcommands(rover, givecommands)
         # Rover.giveroverlocation(rover)
-        self.assertTrue(Rover.readcommands(rover, givecommands, planet), msg=Rover.giveroverlocation(rover))
+        self.assertTrue(Rover.readcommands(self.rover, givecommands, self.planet), msg=Rover.giveroverlocation(self.rover))
 
     def test_will_rover_refuse_unknown_commands(self):
-        planet = Planet(10, 10)
-        rover = Rover(0, 0, 'N', planet)
-
         givecommands = ['s', 'n', 'a', 'r']  # these are the commands that should be executed
         # Rover.readcommands(rover, givecommands)
         # Rover.giveroverlocation(rover)
-        self.assertTrue(Rover.readcommands(rover, givecommands, planet), msg=Rover.giveroverlocation(rover))
+        self.assertTrue(Rover.readcommands(self.rover, givecommands, self.planet), msg=Rover.giveroverlocation(self.rover))
 
     def test_will_rover_refuse_numeric_commands(self):
-        planet = Planet(10, 10)
-        rover = Rover(0, 0, 'N', planet)
-
         givecommands = [3, 2, 5.2, 'r']  # these are the commands that should be executed
         # Rover.readcommands(rover, givecommands)
         # Rover.giveroverlocation(rover)
-        self.assertTrue(Rover.readcommands(rover, givecommands, planet), msg=Rover.giveroverlocation(rover))
+        self.assertTrue(Rover.readcommands(self.rover, givecommands, self.planet), msg=Rover.giveroverlocation(self.rover))
 
 
 if __name__ == '__main__':
